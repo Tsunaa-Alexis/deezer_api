69d3f746eb5141e1ccaf8f4a57375ddba664a9ab
Alexis <55393916+Tsunaa-Alexis@users.noreply.github.com>
2020-11-11 06:04:04 +0100

    ajout d'un artiste

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Track;
+use App\Entity\Artist;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,8 +15,10 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
-        $homepage = file_get_contents('https://api.deezer.com/search/?q=artist:%22damso%22&index=0&limit=0&output=json');
+        $homepage = file_get_contents('https://api.deezer.com/artist/9197980/top?limit=1000');
+        $artist_data = file_get_contents('https://api.deezer.com/artist/9197980');
         $obj = json_decode($homepage,true);
+        $artist_data = json_decode($artist_data,true);
         $obj_array = array();
         $max = sizeof($obj['data']);
         for ($i = 0; $i < $max; $i++) {
@@ -25,9 +28,27 @@ class HomeController extends AbstractController
             array_push($obj_array, $track);
         }
 
+        $artist = new Artist();
+        $artist->setId($artist_data['id']);
+        $artist->setName($artist_data['name']);
+        $artist->setLink($artist_data['link']);
+        $artist->setShare($artist_data['share']);
+        $artist->setPicture($artist_data['picture']);
+        $artist->setPictureSmall($artist_data['picture_small']);
+        $artist->setPictureMedium($artist_data['picture_medium']);
+        $artist->setPictureBig($artist_data['picture_big']);
+        $artist->setPictureXl($artist_data['picture_xl']);
+        $artist->setNmAlbum($artist_data['nb_album']);
+        $artist->setNbFan($artist_data['nb_fan']);
+        $artist->setRadio($artist_data['radio']);
+        $artist->setTracklist($artist_data['tracklist']);
+
+
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
             'obj' => $obj_array,
+            'artist' => $artist,
         ]);
     }
 }
