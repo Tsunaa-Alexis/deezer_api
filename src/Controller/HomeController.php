1986e7028c3f735623f97fbf829e0b53bd96000b
Alexis <55393916+Tsunaa-Alexis@users.noreply.github.com>
2020-11-17 17:19:11 +0100

    compare 2 objet

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -59,4 +59,53 @@ class HomeController extends AbstractController
 
         return $this->redirectToRoute('home', [], 301);
     }
+
+    /**
+     * @Route("/newTrack/{id}", name="newTrack")
+     */
+    public function newTrack($id){
+        $track_db = $this->getDoctrine()->getManager()->getRepository('App:Track')->findby(array('artist' => $id));
+        $track_api = file_get_contents('https://api.deezer.com/artist/'.$id.'/top?limit=1000');
+        $track_api = json_decode($track_api,true);
+        $obj_array = array();
+        $max = sizeof($track_api['data']);
+        for ($i = 0; $i < $max; $i++) {
+            $track = new Track();
+            $track->setId($track_api['data'][$i]['id']);
+            $track->setTitle($track_api['data'][$i]['title']);
+            $track->setLink($track_api['data'][$i]['link']);
+            $track->setDuration($track_api['data'][$i]['duration']);
+            $track->setRank($track_api['data'][$i]['rank']);
+            $track->setArtist($this->getDoctrine()->getManager()->getRepository('App:Artist')->find($id));
+            array_push($obj_array, $track);
+        }
+        
+
+
+
+        return $this->render('Track/newTrack.html.twig', ['track_db' => $track_db, 'track_api' => $obj_array]);
+
+    }
+
+    /**
+     * @Route("/addtrack/{id}", name="addTrack")
+     */
+    public function addTrack($id){
+        $track_data = file_get_contents('https://api.deezer.com/track/'.$id);
+        $track_data = json_decode($track_data,true);
+        $track = new Track();
+        $track->setId($track_data['id']);
+        $track->setTitle($track_data['title']);
+        $track->setLink($track_data['link']);
+        $track->setDuration($track_data['duration']);
+        $track->setRank($track_data['rank']);
+        $track->setArtist($this->getDoctrine()->getManager()->getRepository('App:Artist')->find($track_data['artist']['id']));
+
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($track);
+        $em->flush();
+
+        return $this->redirectToRoute('newTrack', ['id' => $track_data['artist']['id']], 301);
+    }
 }
